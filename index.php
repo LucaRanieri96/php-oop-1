37250537d5e0bd687be28c79b2f7bc67af7e4a5c
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-16 14:38:18 +0200

    Add genre with composition

--- index.php
+++ index.php
@@ -14,10 +14,12 @@ class Movie
 {
   public $title;
   public $description;
-  public function __construct(string $_title, string $_description)
+  public $genre;
+  public function __construct(string $_title, string $_description, Genre $_genre)
   {
     $this->title = $_title;
     $this->description = $_description;
+    $this->genre = $_genre;
   }
   public function getTitle()
   {
@@ -27,11 +29,16 @@ class Movie
   {
     return $this->description;
   }
+  public function getGenre()
+  {
+    return $this->genre;
+  }
 }
 ;
+$genre_1 = new Genre('Fantascienza');
 
-$matrix = new Movie('Matrix', 'Matrix (The Matrix) è un film di fantascienza del 1999 in stile cyberpunk scritto e diretto dalle sorelle Lana e Lilly Wachowski.');
-$avatar = new Movie('Avatar', 'Avatar è un film del 2009 scritto, diretto, co-prodotto e co-montato da James Cameron.');
+$matrix = new Movie('Matrix', 'Matrix (The Matrix) è un film di fantascienza del 1999 in stile cyberpunk scritto e diretto dalle sorelle Lana e Lilly Wachowski.',$genre_1);
+$avatar = new Movie('Avatar', 'Avatar è un film del 2009 scritto, diretto, co-prodotto e co-montato da James Cameron.',$genre_1);
 
 var_dump($matrix);
 var_dump($avatar);
